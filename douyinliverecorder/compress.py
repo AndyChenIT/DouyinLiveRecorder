11790e287db51d636da974e3f8efe7d13c8dfc5f
chenpengyuan <chenpengyuan04@meituan.com>
2024-12-10 19:18:15 +0800

    压缩视频

--- douyinliverecorder/compress.py
+++ douyinliverecorder/compress.py
@@ -3,7 +3,7 @@ from ftplib import print_line
 
 from ffmpeg_progress_yield import FfmpegProgress
 
-def compress_video(input_file, output_file, crf=36, preset='slower'):
+def compress_video(input_file, output_file, crf=28, preset='fast'):
     """
     使用 ffmpeg 压缩视频
     :param input_file: 输入视频文件路径
@@ -24,15 +24,13 @@ def compress_video(input_file, output_file, crf=36, preset='slower'):
     ]
 
     print("压缩任务------------------------------start")
-
     ff = FfmpegProgress(command)
     for progress in ff.run_command_with_progress():
         print(f"\r压缩进度: {progress:.2f}%", end='', flush=True)
-
-    print("压缩任务------------------------------end")
+    print("\n压缩任务------------------------------end")
 
 
 if __name__ == '__main__':
-    input_video = '/Users/andychen/software/programSoftwareSpace/PycharmSpace/DouyinLiveRecorder/downloads/抖音直播/ChiPin/ChiPin_做小红书：如何出爆款、接广告_2024-11-07_10-13-07.mp4'
-    output_video = '/Users/andychen/software/programSoftwareSpace/PycharmSpace/DouyinLiveRecorder/downloads/抖音直播/ChiPin/ChiPin_做小红书：如何出爆款、接广告_2024-11-07_10-13-07-压缩.mp4'
+    input_video = '/Users/andychen/software/programSoftwareSpace/PycharmSpace/DouyinLiveRecorder/downloads/抖音直播/ChiPin/ChiPin_做小红书：如何出爆款、接广告_2024-12-10_08-15-37.mp4'
+    output_video = '/Users/andychen/software/programSoftwareSpace/PycharmSpace/DouyinLiveRecorder/downloads/抖音直播/ChiPin/ChiPin_做小红书：如何出爆款、接广告_2024-12-10_08-15-37-压缩.mp4'
     compress_video(input_video, output_video)
